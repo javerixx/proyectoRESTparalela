e76aea1a32de6e32bdd15110008bc5068e019ff1
Sebastron <sebastiang2806@gmail.com>
2020-06-25 19:39:28 -0400

    Pruebas, clase carreras

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -4,11 +4,16 @@ const rutas = Router();
 
 const carreras = require('../carreras.json');
 
+//Funciones que se usan
+
 
 //rutas de la aplicación
 rutas.get('/', (req, res) => {
-    //req.params(name, tipomime);
-    console.log(carreras[0]);
+    req.params(name, tipomime, context);
+    // Nombre del archivo, tipo mime del archivo y contenido del archivo
+    for(var i = 0; i<28; i++){
+        console.log(carreras[i].nombre_carrera);
+    }
     res.json(carreras);
 });
 
